0400b2ab1ae34db1dc89df593ad7ca3fa7757e9d
qiuyufan <qiuyufan@example.com>
2025-11-07 13:07:31 +0200

    dl useless file

--- models/intermediate/int_ad_network_1_performance.sql
+++
@@ -1,6 +0,0 @@
-select * from {{ ref('stg_ad_network_1_detailed_report') }}
-union all
-select * from {{ ref('stg_ad_network_1_country_report') }}
-where country_id not in (
-    select distinct country_id from {{ ref('stg_ad_network_1_detailed_report') }}
-)
