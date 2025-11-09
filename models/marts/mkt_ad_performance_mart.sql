ebfee4927bb46d801501b96061faf694fb896d50
qiuyufan <qiuyufan@example.com>
2025-11-09 09:45:19 +0200

    refactored mart. moved the added network column to int layer

--- models/marts/mkt_ad_performance_mart.sql
+++ models/marts/mkt_ad_performance_mart.sql
@@ -1,43 +1,5 @@
 {{ config(materialized='table') }}
 
--- ============================================================
--- Marketing Ad Performance Datamart
---
--- Purpose:
---   Unified view of Ad Network 1 and Ad Network 2 performance
---   at the most granular geo level available.
--- ============================================================
-
-with ad1 as (
-    select
-        'ad_network_1' as ad_network,
-        report_date,
-        campaign_id,
-        campaign_name,
-        country_name,
-        country_code,
-        state_name,
-        spend,
-        impressions,
-        clicks
-    from {{ ref('int_ad_network_1_combined') }}
-),
-
-ad2 as (
-    select
-        'ad_network_2' as ad_network,
-        report_date,
-        campaign_id,
-        campaign_name,
-        country_name,
-        country_code,
-        null as state_name,
-        spend,
-        impressions,
-        clicks
-    from {{ ref('int_ad_network_2_geo_joined') }}
-)
-
-select * from ad1
+select * from {{ ref('int_ad_network_1_combined') }}
 union all
-select * from ad2
+select * from {{ ref('int_ad_network_2_geo_joined') }}
